5bb8bf65ce2f41f66137a7c928e7782a4bb29868
jacarrichan <772929989@qq.com>
2021-12-12 21:54:13 +0800

    init

---
+++ service-feign/src/main/java/com/jacarrichan/demo/feign/IndexController.java
@@ -0,0 +1,37 @@
+package com.jacarrichan.demo.feign;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @Resource
+    private HelloFeignClient helloFeignClient;
+    @Resource
+    private UserFeignClient userFeignClient;
+    @Resource
+    private IpFeignClient ipFeignClient;
+
+    @GetMapping(value = "/index")
+    public String hello() {
+        System.out.println("Enter hello world service!");
+        return helloFeignClient.getHelloContent();
+    }
+
+    @GetMapping(value = "/user")
+    public String user() {
+        System.out.println("Enter hello world service!");
+        return userFeignClient.getHelloContent();
+    }
+
+    @GetMapping(value = "/ip")
+    public String ip() {
+        System.out.println("Enter hello world service!");
+        return ipFeignClient.json();
+    }
+}
